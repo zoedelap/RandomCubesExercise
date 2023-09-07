5b7a452c68d6c2cd965987c06c4b2a9f9f259825
Zoe Delap <73131522+zoedelap@users.noreply.github.com>
2023-09-07 11:09:30 -0500

    Added cube spawning, color changing, and destroying

--- RandomCubesExercise/Assets/Scripts/RandomCubeExercise.cs
+++ RandomCubesExercise/Assets/Scripts/RandomCubeExercise.cs
@@ -6,17 +6,14 @@ public class RandomCubeExercise : MonoBehaviour
 {
 
     [SerializeField]
-    private GameObject cubePrefab;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private GameObject cubePrefabVar;
 
     // Update is called once per frame
     void Update()
     {
-        Instantiate(cubePrefab);
+        GameObject cubeGO = Instantiate(cubePrefabVar);
+        Material cubeMat = cubeGO.GetComponent<Renderer>().material;
+        cubeMat.color = Random.ColorHSV(0, 1, 0.5f, 1, 0.75f, 1);
+        Object.Destroy(cubeGO, 1.5f);
     }
 }
