476bbee41d4aec470c6550882725845c8ee17fd6
Zoe Delap <73131522+zoedelap@users.noreply.github.com>
2023-09-07 12:09:12 -0500

    Added random sizes

--- RandomCubesExercise/Assets/Scripts/RandomCubeExercise.cs
+++ RandomCubesExercise/Assets/Scripts/RandomCubeExercise.cs
@@ -11,9 +11,11 @@ public class RandomCubeExercise : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GameObject cubeGO = Instantiate(cubePrefabVar);
-        Material cubeMat = cubeGO.GetComponent<Renderer>().material;
+        GameObject cube = Instantiate(cubePrefabVar);
+        float cubeWidth = Random.Range(0.1f, 1);
+        cube.transform.localScale = new Vector3(cubeWidth, cubeWidth, cubeWidth);
+        Material cubeMat = cube.GetComponent<Renderer>().material;
         cubeMat.color = Random.ColorHSV(0, 1, 0.5f, 1, 0.75f, 1);
-        Object.Destroy(cubeGO, 1.5f);
+        Object.Destroy(cube, 1.5f);
     }
 }
